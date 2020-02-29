a6d6e06646ab5df34a07cc16c2bc53bd99bad32c
Arnaud TANGUY <arn.tanguy@gmail.com>
2020-02-29 22:17:26 +0900

    [clang-format] Add and run clang-format scripts
    No code change, only formatting

--- include/mesh_sampling/assimp_scene.h
+++ include/mesh_sampling/assimp_scene.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <assimp/postprocess.h>  // Post processing flags
-#include <assimp/scene.h>        // Output data structure
-#include <assimp/Importer.hpp>   // C++ importer interface
+#include <assimp/Importer.hpp> // C++ importer interface
+#include <assimp/postprocess.h> // Post processing flags
+#include <assimp/scene.h> // Output data structure
 
 namespace mesh_sampling
 {
@@ -12,17 +12,19 @@ class ASSIMPScene
   // Create an instance of the Importer class
   // The importer will automatically delete the scene
   Assimp::Importer importer;
-  const aiScene *scene_;
+  const aiScene * scene_;
 
- public:
-  ASSIMPScene(const std::string& model_path) :
-    // And have it read the given file with some example postprocessing
+public:
+  ASSIMPScene(const std::string & model_path)
+  : // And have it read the given file with some example postprocessing
     // Usually - if speed is not the most important aspect for you - you'll
     // propably to request more postprocessing than we do in this example.
-    scene_(importer.ReadFile(model_path, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_GenNormals | aiProcess_FixInfacingNormals))
+    scene_(importer.ReadFile(model_path,
+                             aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_GenNormals
+                                 | aiProcess_FixInfacingNormals))
   {
     // If the import failed, report it
-    if (!scene_)
+    if(!scene_)
     {
       throw std::runtime_error(importer.GetErrorString());
     }
@@ -33,11 +35,10 @@ class ASSIMPScene
    *
    * @return
    */
-  const aiScene* scene() const
+  const aiScene * scene() const
   {
     return scene_;
   }
-
 };
 
-} /* mesh_sampling */
+} // namespace mesh_sampling
