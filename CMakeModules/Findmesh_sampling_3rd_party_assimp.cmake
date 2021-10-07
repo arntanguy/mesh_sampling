04de4f3cff64ec66f1d280656f1dfbc213fe8248
Arnaud TANGUY <arn.tanguy@gmail.com>
2021-10-07 15:31:55 +0200

    [ci] Drop Ubuntu 16.04 support

--- CMakeModules/Findmesh_sampling_3rd_party_assimp.cmake
+++ CMakeModules/Findmesh_sampling_3rd_party_assimp.cmake
@@ -2,29 +2,22 @@
 # Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
 #
 
-# Try to find the GEOS library
+# Try to find the ASSIMP library
 #
-# In order:
-# - try to find_package(geos) (upstream package)
-# - try to use geos-config to find the geos installation prefix
-# - try to find the geos library
+# If the library is found, then you can use the mesh_sampling::assimp target
 #
-# If the library is found, then you can use the PCL::PCL target
-#
-# This module will use GEOS_PREFIX as an hint to find either the geos-config
-# executable or the geos library
 
 if(NOT TARGET mesh_sampling::assimp)
   message(STATUS "Checking for module 'ASSIMP'")
   find_package(assimp QUIET)
-  if(NOT ${ASSIMP_FOUND})
+  if(NOT ${assimp_FOUND})
     message(FATAL_ERROR "Could not find the ASSIMP library")
-  else()
-    message(STATUS "Found assimp library: ${ASSIMP_LIBRARIES}")
-    add_library(mesh_sampling::assimp INTERFACE IMPORTED)
-    set_target_properties(mesh_sampling::assimp PROPERTIES
-      #INTERFACE_INCLUDE_DIRECTORIES ${ASSIMP_INCLUDE_DIRS}
-      INTERFACE_LINK_LIBRARIES "${ASSIMP_LIBRARIES}"
-    )
   endif()
+
+  message(STATUS "Found assimp library: ${assimp_VERSION_MAJOR}.${assimp_VERSION_MINOR}.${assimp_VERSION_PATCH}")
+  add_library(mesh_sampling::assimp INTERFACE IMPORTED)
+  set_target_properties(mesh_sampling::assimp PROPERTIES
+    #INTERFACE_INCLUDE_DIRECTORIES ${ASSIMP_INCLUDE_DIRS}
+    INTERFACE_LINK_LIBRARIES "${ASSIMP_LIBRARIES}"
+  )
 endif()
